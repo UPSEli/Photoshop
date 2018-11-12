96a9f13ab05f624f01922bf43ef899db0fed1c65
rishiiups <43057397+rishiiups@users.noreply.github.com>
2018-11-12 11:26:22 -0800

    11/12
    you can now select a photo

--- Main.java
+++ Main.java
@@ -13,54 +13,83 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
 
 
 
 public class Main extends Application {
 
     Stage window;
+    File file;
+    TextField path_field;
+    StackPane root_photo;
+    Image image;
+    ImageView iv;
+    Scene scene_photo;
 
     public static void main(String[] args){
         launch(args);
     }
 
+    public void loadphoto(){
+        String u = "";
+        try {
+            String input = path_field.getText();
+            u = input;
+            image = new Image(input);
+            iv = new ImageView(image);
+
+            root_photo.getChildren().add(iv);
+            window.setScene(scene_photo);
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("Didnt find "+ u);
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("CSCI 240 Group Project");
+        window = primaryStage;
+        window.setTitle("spagheGUI");
 
-        StackPane root_photo = new StackPane();
-        Scene scene_photo = new Scene(root_photo, 900, 800);
+        root_photo = new StackPane();
+        scene_photo = new Scene(root_photo, 900, 800);
 
-        Label pre_text = new Label("Path to file:");
-        TextField textField = new TextField ();
-        GridPane gb = new GridPane();
-        GridPane.setConstraints(pre_text, 0,0);
-        //hb.setSpacing(10);
+        GridPane HOMEGRID = new GridPane();
+        HOMEGRID.setPadding(new Insets(10,10,10,10));
+        HOMEGRID.setVgap(8);
+        HOMEGRID.setHgap(10);
 
-        StackPane root_start = new StackPane();
-        Scene scene_start= new Scene(gp, 500, 500);
+        Label path_label = new Label("Path to file:");
+        GridPane.setConstraints(path_label, 0, 0);
 
+        path_field = new TextField ("photo.jpg");
+        GridPane.setConstraints(path_field, 1, 0);
+
+        Scene scene_start= new Scene(HOMEGRID, 500, 500);
 
 
         Button switch_button = new Button("See photo");
-        switch_button.setOnAction(e-> primaryStage.setScene(scene_photo));
+        switch_button.setOnAction(e -> loadphoto());
+        GridPane.setConstraints(switch_button, 0, 1);
 
         Button switch_button2 = new Button("Go back");
-        switch_button2.setOnAction(f-> primaryStage.setScene(scene_start));
+        switch_button2.setOnAction(f-> window.setScene(scene_start));
+
+        //image = new Image("treebg.jpg");
+        //iv = new ImageView(image);
+
+        //root_photo.getChildren().add(iv);
 
 
 
-        File file = new File("C:\\Users\\rk\\Desktop\\hDesktop\\spacex-falcon-heavy-launch.jpg");
-        Image image = new Image(file.toURI().toString());
-        ImageView iv = new ImageView(image);
-
-        root_photo.getChildren().add(iv);
         root_photo.getChildren().add(switch_button2);
+        HOMEGRID.getChildren().addAll(path_label, path_field, switch_button);
 
-        hb.getChildren().add(switch_button);
 
         primaryStage.setScene(scene_start);
         primaryStage.show();
     }
+
 
 }
